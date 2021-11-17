1fe62d86e7d4435098bafed79522206008b136ed
huuhuybnPC <huuhuybn@gmail.com>
2021-11-17 08:18:52 +0700

    Initial commit

---
+++ app/src/main/java/com/poly/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.poly.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+// hong tien
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
