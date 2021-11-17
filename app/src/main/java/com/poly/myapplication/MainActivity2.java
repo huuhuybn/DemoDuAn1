d729d6315cfb41c1dc6f5ee52276c3e85ee7432c
huuhuybnPC <huuhuybn@gmail.com>
2021-11-17 08:25:15 +0700

    test

--- app/src/main/java/com/poly/myapplication/MainActivity2.java
+++ app/src/main/java/com/poly/myapplication/MainActivity2.java
@@ -3,6 +3,8 @@ package com.poly.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 // Dai An
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        TextView yb;
     }
 }
